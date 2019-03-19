57138c5cc4efc600a6ea7020e10f2a1da759ee76
unknown <bustamante.emanuel@gmail.com>
2019-03-19 14:30:14 -0700

    Found a better way to make change

--- src/main/java/com/lhlic/vendingMachine/change/ChangeMaker.java
+++ src/main/java/com/lhlic/vendingMachine/change/ChangeMaker.java
@@ -1,14 +1,26 @@
 package com.lhlic.vendingMachine.change;
 
-public class ChangeMaker {
-	public static int howMany(float amount, Unit unit) {
-		// What is the amount given in cents?
-		int cents = (int) (Math.ceil(amount * 100));
+import java.util.HashMap;
+import java.util.Map;
 
+public class ChangeMaker {	
+	public static Map<Unit, Integer> makeChange(float payment){
+		// Convert the payment to cents
+		int paymentAsCents = (int) (Math.ceil(payment * 100));
+		// Map each unit to the number of units need to make change
+		Map<Unit, Integer> change = new HashMap<Unit, Integer>();
 		
-		// How many of the unit are in the given amount?
-		int howManyFit = cents / unit.asCents();
+		// Calculate how much of each unit is need
+		int howMany = 0;
+		for(Unit unit: Unit.values()) {
+			// How many of the unit fit in the payment
+			howMany = paymentAsCents / unit.asCents();
+			change.put(unit, howMany);
+			
+			// How much is left after the unit
+			paymentAsCents %= unit.asCents();
+		}
 		
-		return howManyFit;
+		return change;
 	}
 }
--- src/test/java/com/lhlic/vendingMachine/change/ChangeMakerTest.java
+++ src/test/java/com/lhlic/vendingMachine/change/ChangeMakerTest.java
@@ -2,51 +2,22 @@ package com.lhlic.vendingMachine.change;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+import java.util.Random;
+
 import org.junit.Test;
 
 
 public class ChangeMakerTest {
-	public static final float CENT_MOE = .001f;
-	
 	@Test
-	public void dollarConversion() {
-		assert Unit.TWENTY.asDollars() == 20.00;
-		assert Unit.TEN.asDollars() == 10.00;
-		assert Unit.FIVE.asDollars() == 5.00;
-		assert Unit.ONE.asDollars() == 1.00;
-		assertEquals(Unit.QUARTER.asDollars(), .25, CENT_MOE);
-		assertEquals(Unit.DIME.asDollars(), .1, CENT_MOE);
-		assertEquals(Unit.NICKEL.asDollars(), .05, CENT_MOE);
-		assertEquals(Unit.PENNY.asDollars(), .01, CENT_MOE);
-	}
-	
-	
-	@Test
-	// Giving zero dollars should result in no change
-	public void noChange() {
-		float amount = 0;
-		for(Unit unit: Unit.values()) {
-			assert ChangeMaker.howMany(amount, unit) == 0;
+	// Check for mixture of change
+	public void mixed() {
+		float payment = 7.00f;
+		Map<Unit, Integer> change = ChangeMaker.makeChange(payment);
+		for(Unit unit: change.keySet()) {
+			System.out.println(unit + " -> " + change.get(unit));
 		}
 	}
-	
-	@Test
-	// Check a unit will be returned as changed
-	// I.e. ten dollars in ten dollar bills is 1
-	public void unitAsChange() {
-		for(Unit unit: Unit.values()) {
-			assertEquals(1, ChangeMaker.howMany(unit.asDollars(), unit));
-		}
-	}
-	
-	@Test
-	// Check multiple of a single unit
-	public void oneUnitMultiple() {
-		for(int i = 0; i < 100) {
-			
-		}
-	}
-	
 	
 	/**
 	 * Error cases
